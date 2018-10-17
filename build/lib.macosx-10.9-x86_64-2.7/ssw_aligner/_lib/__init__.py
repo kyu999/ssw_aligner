0503754bdd497d5846dde062d997c5dfa5961293
kyu999 <kyukokkyou999@gmail.com>
2018-10-17 09:40:52 +0900

    ignore build, dist, and egg-info

--- build/lib.macosx-10.9-x86_64-2.7/ssw_aligner/_lib/__init__.py
+++
@@ -1,7 +0,0 @@
-# ----------------------------------------------------------------------------
-# Copyright (c) 2013--, ssw_aligner development team.
-#
-# Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file COPYING.txt, distributed with this software.
-# ----------------------------------------------------------------------------
